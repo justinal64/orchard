0173f792d2b8358aa9cb7dfb199839511d7f110e
justin leggett <justin@codingrookie.com>
2016-10-08 13:33:05 -0500

    completed the tree exercise

--- javascript/es6.js
+++ javascript/es6.js
@@ -15,7 +15,7 @@ console.log("b", b);
 function varTest() {
     var x = 1;
     if(true) {
-        var x = 2;
+        // var x = 2;
         console.log("inside if x ", x);
     }
     console.log("outside if x ", x);
@@ -38,26 +38,26 @@ letTest();
 var sum = function() {
     let a = 1, b = 2;
     return a + b;
-}
-console.log("hardcore sum in EX5", sum());;
+};
+console.log("hardcore sum in EX5", sum());
 
 // this is es6 shorthand for the function above
 var sum2 = () => {
     let a = 1, b = 2;
     return a + b;
-}
+};
 console.log("hardcoded sum in ES6", sum());
 
 
 var reflect = (value) => {
-    return value
-}
+    return value;
+};
 // var reflect = (value) => value; // super shorthand for the above ES6
 console.log("reflect in ES6", reflect("ES6 is the new hotness"));
 
 var realSum = (num1, num2) => {
     return num1 + num2;
-}
+};
 console.log("realSum ES6", realSum(2, 3));
 
 // string templates
@@ -69,32 +69,32 @@ console.log("r = ", r);
 
 // object literal property shorthand
 
-let wow = "Hi these", es6 = "ES6", numNum = ()=>{console.log("howdy")}
+let wow = "Hi these", es6 = "ES6", numNum = ()=>{console.log("howdy");};
 
 let myOldObject = {
     wow:wow,
     es6:es6,
     numNum: numNum
-}
+};
 
 console.log("myOldObject", myOldObject);
 let myNewObject = {
     wow,es6,numNum
-}
+};
 console.log("myNewObject", myNewObject);
 
 // method properties
 
 var e5Obj = {
-    foo:function(){console.log("foo")},
-    bar:function() {console.log("bar")}
-}
+    foo:function(){console.log("foo");},
+    bar:function() {console.log("bar");}
+};
 console.log("e5Obj", e5Obj);
 
 var Es6Obj = {
-    foo(){console.log("foo")},
-    bar(){console.log("bar")}
-}
+    foo(){console.log("foo");},
+    bar(){console.log("bar");}
+};
 console.log("Es6Obj", Es6Obj);
 
 
